3c8d77023a81e5b88cadd5f197890416610f44ee
Venice Juanillas <v.juanillas@irri.org>
2018-01-22 02:01:06 +0000

    Troubleshoot TASSEL kinship

---
+++ tools/tassel5/calcPCA.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#configuration
+WORK_DIR=`pwd`
+TASSEL_dir="/home/galaxy/galaxy-prod/tools/tassel5"
+TMP_DIR=$TASSEL_DIR"/tmp"
+
+# run these Unix commands to preprocess files
+mkdir $TMP_DIR
+
+
